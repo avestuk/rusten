0640582e64e655c36a8d4017291848c317956f11
Alex Vest <alex.vest@nutmeg.com>
2022-07-06 20:14:30 +0100

    Clean things up a bit with slices and usize

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -7,19 +7,19 @@ fn main() {
         .read_line(&mut n)
         .expect("Failed to read the line!");
 
-    let n = n.trim().parse::<u32>().expect("failed to parse");
+    let n = n.trim().parse::<usize>().expect("failed to parse");
     println!("the {}th number in fibaonacci seq is: {}", n, fibonacci(n))
 }
 
-fn fibonacci(n: u32) -> u32 {
-    let mut fib: Vec<u32> = vec![0, 1];
-    if n < (2_u32) {
+fn fibonacci(n: usize) -> usize {
+    let mut fib: Vec<usize> = vec![0, 1];
+    if n < (2_usize) {
         return fib[n as usize];
     }
     for i in 2..=n as usize {
         fib.push(fib[i - 1] + fib[i - 2]);
     }
 
-    println!("fib: {:?}", fib);
-    fib[n as usize]
+    println!("fib: {:?}", &fib[1..]);
+    fib[n]
 }
