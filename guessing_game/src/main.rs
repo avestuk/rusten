1c66d65fb6c1b97f4181d4e74549cb7e49f82e9e
Alex Vest <alex.vest@nutmeg.com>
2023-03-02 20:26:55 +0000

    Use type
    Nice way of ensuring input is correct

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,34 @@
+use core::fmt;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl fmt::Display for Guess {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+impl Guess {
+    pub fn new(i: i32) -> Guess {
+        // This is a great construct!
+        if !(0..=100).contains(&i) {
+            panic!("Guess must be between 1 and 100, got: {}", i)
+        }
+        Guess { value: i }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
-    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
 
     println!("Please enter a guess between 1 and 100");
 
@@ -17,8 +42,8 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read the line!");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
@@ -26,7 +51,7 @@ fn main() {
 
         println!("You guessed {}", guess);
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("You guessed too low"),
             Ordering::Greater => println!("You guessed too high"),
             Ordering::Equal => {
